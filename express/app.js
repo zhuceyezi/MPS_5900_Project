9a85ebe66c76aec324a4944326098d4920b67beb
zhuceyezi <hln1234567890@qq.com>
2024-10-01 17:50:34 -0400

    Re-structured folders based on architecture

--- express/app.js
+++ express/app.js
@@ -4,14 +4,14 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./apis");
-var usersRouter = require("./apis/users");
+var indexRouter = require("./apiServices");
+var usersRouter = require("./apiServices/users");
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
--- express/models/Employee.js
+++ express/models/Employee.js
@@ -1,8 +1,8 @@
 const database = require("../config/databaseSetUp").database;
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require("sequelize");
 
 /**
- * 
+ *
  * @params ID AUTO NOT NULL
  * @params imageId NOT NULL,
  * @params name NOT NULL,
@@ -10,32 +10,32 @@ const { DataTypes } = require("sequelize");
  * @params EmployeeId
  */
 const Employee = database.define("Employee", {
-  // Model attributes are defined here
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  employeeId: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  employeeName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  imageId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastLogin: {
-    type: DataTypes.DATE,
-    allowNull: true,
-    defaultValue: null,
-  }
-  // This feild is used to store the employee id from company database system
-  
-},{underscored: true});
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    employeeId: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    employeeName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    imageId: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastLogin: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null
+    }
+    // This feild is used to store the employee id from company database system
+    
+}, {underscored: true});
 
 module.exports = Employee;
