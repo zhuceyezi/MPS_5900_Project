daba826d9ec87d868bbadc8bdeaf7540a7556fac
zhuceyezi <hln1234567890@qq.com>
2024-10-01 17:56:27 -0400

    fixed explicit password log

--- express/config/databaseSetUp.js
+++ express/config/databaseSetUp.js
@@ -1,24 +1,23 @@
-const { Sequelize } = require("sequelize");
+const {Sequelize} = require("sequelize");
 require("dotenv").config("../../.env");
 
 // create a new instance to connect to the database
 // use .env file to store sensitive data for best practices
-console.log(process.env.DB_PASSWORD);
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
 );
 
 exports.database = sequelize;
