5495ee72e3edee47aaff454c6b15fd3578295477
Yry01 <103943433+Yry01@users.noreply.github.com>
2024-10-25 23:45:40 -0400

    fix the fetch logic in register page and add logic in frontend

--- express/public/js/register.js
+++ express/public/js/register.js
@@ -7,21 +7,19 @@ const captureButton = document.getElementById("capture");
 const submitButton = document.getElementById("submit");
 const retakeButton = document.getElementById("retake");
 
-const usernameInput = document.getElementById("username"); 
-const useridInput = document.getElementById("userid");     
-
-
+const usernameInput = document.getElementById("username");
+const useridInput = document.getElementById("userid");
 
 
 // Access the user's camera and stream it to the video element
 navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-        video.srcObject = stream;
-    })
-    .catch((err) => {
-        console.error("Error accessing camera: " + err);
-    });
+         .getUserMedia({video: true})
+         .then((stream) => {
+             video.srcObject = stream;
+         })
+         .catch((err) => {
+             console.error("Error accessing camera: " + err);
+         });
 
 
 // Capture the photo from the live camera feed
@@ -56,13 +54,13 @@ captureButton.addEventListener("click", () => {
 retakeButton.addEventListener("click", () => {
     // Restart the video stream
     navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-            video.srcObject = stream;
-        })
-        .catch((err) => {
-            console.error("Error accessing camera: " + err);
-        });
+             .getUserMedia({video: true})
+             .then((stream) => {
+                 video.srcObject = stream;
+             })
+             .catch((err) => {
+                 console.error("Error accessing camera: " + err);
+             });
 
     capturedPhoto.style.display = "none";
     video.style.display = "block";
@@ -88,46 +86,47 @@ form.addEventListener('submit', (event) => {
 
     console.log("User Name:", username);
     console.log("User ID:", userid);
-    
+
     // Add logic to send data to the server as needed
-   //convert canvas to blob
-    canvas.toBlob(async(blob) =>{
-        try{
+    //convert canvas to blob
+    canvas.toBlob(async (blob) => {
+        try {
             await sendImageToAPI(blob);
-            } catch(error) {
-                console.error("Error sending image:", error);
-            }
-            }, `image/jpeg`, 0.95);
+        } catch (error) {
+            console.error("Error sending image:", error);
+            alert("Error in registering user. Please try again.");
+        }
+    }, `image/jpeg`, 0.95);
 
 });
 
 
-    
 // Function to send the image data to the API
 async function sendImageToAPI(imageBlob) {
     try {
         const formData = new FormData();
         formData.append("image", imageBlob, "photo.jpg");
-        formData.append("username", usernameInput.value);
-        formData.append("userid", useridInput.value);
+        formData.append("employeeName", usernameInput.value);
+        formData.append("employeeId", useridInput.value);
 
         console.log("Sending photo and user info...");
 
-        const response = await fetch("http://localhost:3000/facial/validate", {
+        const response = await fetch("http://localhost:3000/facial", {
             method: "POST",
             body: formData,
-            redirect: "follow",
+            redirect: "follow"
         });
 
-        if (response.status ===200) {
+        if (response.status === 201) {
             const result = await response.json();
-            console.log("Data sent successfully:", result);     
-        }else{
+            console.log("Data sent successfully:", result)
+            window.location.href = "/express/public/index.html";
+        } else {
             const errorMessage = await response.json();
             console.log("Data sent failed:", errorMessage);
         }
-    
+
     } catch (error) {
-        console.error("Error during API call:", error.message); 
+        console.error("Error during API call:", error.message);
     }
 }
